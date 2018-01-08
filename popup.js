1a3c54cc3df197a44956cead11a415b011bf9308
zezzer <zemeizeng@gmail.com>
2018-01-08 15:48:27 -0800

    Added radio buttons for meridian and basic error checking for time

--- popup.js
+++ popup.js
@@ -1,19 +1,55 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-	var hours = localStorage.getItem("hour");
-	if(hours) {
-		hours = JSON.parse(hours);
-		for(i = 0; i < hours.length; i++) {
-			addReminder(hours, hours[i]);
+	var entries = localStorage.getItem("entries");
+	if(entries) {
+		entries = JSON.parse(entries);
+		for(i = 0; i < entries.length; i++) {
+			addReminder(entries, entries[i]);
 		}
 	}
 	document.getElementById("create-reminder").addEventListener("click", function() {
-		if(!hours) 
-			hours = [];
-		var hour_elem = document.getElementById("hour");
-		hours.push(hour_elem.value);
-		localStorage.setItem("hour", JSON.stringify(hours));
-		addReminder(hours, hour_elem.value);
-		hour_elem.value = "";
+		if(!entries) 
+			entries = [];
+		var hour = document.getElementById("hour").value;
+		var minute = document.getElementById("minutes").value;
+		var note = document.getElementById("note").value;
+		var meridiem;
+
+		if(document.getElementById("am").checked)
+			meridiem = "AM"
+		else
+			meridiem = "PM";
+
+		var flag = 0;
+		if(hour.match(/^[0-9]+$/) === null || hour > 12 || hour < 0) {
+			document.getElementById("hour").style.border = "solid 1px red";
+			flag = 1;
+		}
+		else {
+			hour = parseInt(hour, 10);
+			document.getElementById("hour").style.border = "";
+		}
+		if(minute.match(/^[0-9]+$/) === null || minute > 60 || minute < 0) {
+			document.getElementById("minutes").style.border = "solid 1px red";
+			flag = 1;
+		}
+		else {
+			minute = parseInt(minute, 10);
+			document.getElementById("minutes").style.border = "";
+		}
+		if(flag)
+			return;
+
+		if(minute<10)
+			minute = "0" + minute;
+
+		var entry = [hour, minute, meridiem, note];
+		entries.push(entry);
+		localStorage.setItem("entries", JSON.stringify(entries));
+		addReminder(entries, entry);
+
+		document.getElementById("hour").value = "";
+		document.getElementById("minutes").value = "";
+		document.getElementById("note").value = "";
 	});
 	document.getElementById("clear").addEventListener("click", function() {
 		window.localStorage.clear();
@@ -21,28 +57,27 @@ document.addEventListener("DOMContentLoaded", function(event) {
 		while(saved.firstChild) {
 			saved.removeChild(saved.firstChild);
 		}
-		document.style.height="300px"
 	});
 });
 
-function addReminder(hours, val) {
+function addReminder(entries, entry) {
 	var saved = document.getElementById("saved");
 	var child = document.createElement("div");
-	child.innerHTML = val;
+	child.innerHTML = entry[0] + ":" + entry[1] + " " + entry[2] + " " + entry[3] + " ";
 	saved.appendChild(child);
 
 	var remove = document.createElement("button");
 	remove.class = "remove";
 	remove.innerHTML = "X"
 	remove.addEventListener("click", function() {
-		var idx = hours.indexOf(val);
+		var idx = entries.indexOf(entry);
 		var node = saved.childNodes[idx];
-		saved.removeChild(node);
 		while(node.firstChild) {
 			node.removeChild(node.firstChild);
 		}
-		hours.splice(idx, 1);
-		localStorage.setItem("hour", JSON.stringify(hours));
+		saved.removeChild(node);
+		entries.splice(idx, 1);
+		localStorage.setItem("entries", JSON.stringify(entries));
 	});
 	child.appendChild(remove);
 }
